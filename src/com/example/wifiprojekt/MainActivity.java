7c06b394166409376e49ec7f81dcc2a35535e3a3
dominicanodennis <dominicanodennis@hotmail.de>
2015-02-09 16:05:53 +0100

    "SSID und level funzt "

--- src/com/example/wifiprojekt/MainActivity.java
+++ src/com/example/wifiprojekt/MainActivity.java
@@ -2,20 +2,21 @@ package com.example.wifiprojekt;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import java.util.List;
 
+
+
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
-import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -26,8 +27,9 @@ public class MainActivity extends ActionBarActivity {
 	WifiScanReceiver wifiReciever;
 	ListView list;
 	String wifis[];
+	Toast toast;
 
-
+	@SuppressLint("ShowToast")
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
@@ -35,12 +37,35 @@ public class MainActivity extends ActionBarActivity {
 		mainWifiObj = (WifiManager) getSystemService(Context.WIFI_SERVICE);
 		wifiReciever = new WifiScanReceiver();
 		mainWifiObj.startScan();
+
+		toast = Toast.makeText(this, "Bitte klicken das gewünschte Wlannetz",
+				5000);
+		toast.setGravity(Gravity.CENTER, 0, 75);
+		toast.show();
+
+	}
+	public void restartActivity(){
+		finish();
+		startActivity(getIntent());
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// TODO Auto-generated method stub
-		return super.onCreateOptionsMenu(menu);
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			restartActivity();
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
 	}
 
 	protected void onPause() {
@@ -60,7 +85,7 @@ public class MainActivity extends ActionBarActivity {
 			List<ScanResult> wifiScanList = mainWifiObj.getScanResults();
 			wifis = new String[wifiScanList.size()];
 			for (int i = 0; i < wifiScanList.size(); i++) {
-				wifis[i] = ((wifiScanList.get(i)).toString());
+				wifis[i] = ((wifiScanList.get(i)).SSID.toString()+ " "+ wifiScanList.get(i).level);
 			}
 
 			list.setAdapter(new ArrayAdapter<String>(getApplicationContext(),
