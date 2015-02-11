ee4f6bf5c574981fe8eba379bd9eca17ced85f88
dominicanodennis <dominicanodennis@hotmail.de>
2015-02-11 14:49:51 +0100

    "testcommit für ole "

--- src/com/example/wifiprojekt/MainActivity.java
+++ src/com/example/wifiprojekt/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.wifiprojekt;
 
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.internal.widget.AdapterViewCompat.OnItemLongClickListener;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import java.util.List;
 
@@ -15,16 +17,18 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements
+		android.widget.AdapterView.OnItemLongClickListener {
 
-	WifiManager WifiManagerObjekt;
-	WifiScannerReceiver wifiReceiver;
-	ListView list;
-	String wifiListe[];
+	WifiManager wifimanager;
+	WifiReceiver wifiReceiver;
+	ListView listview;
+	String wifiliste[];
 	Toast toast1, toast2, toast3;
 
 	@SuppressLint("ShowToast")
@@ -32,26 +36,28 @@ public class MainActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		list = (ListView) findViewById(R.id.listView1);
-		WifiManagerObjekt = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+		listview = (ListView) findViewById(R.id.listView1);
+		wifimanager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
 
-		if (!WifiManagerObjekt.isWifiEnabled()) {
+		if (!wifimanager.isWifiEnabled()) {
 			toast2 = Toast.makeText(this, "Wlannetze werden gesucht", 5000);
 			toast2.setGravity(Gravity.CENTER, 0, 75);
 			toast2.show();
-			WifiManagerObjekt.setWifiEnabled(true);
+			wifimanager.setWifiEnabled(true);
 		}
 
-		wifiReceiver = new WifiScannerReceiver();
-		WifiManagerObjekt.startScan();
+		wifiReceiver = new WifiReceiver();
+		wifimanager.startScan();
 
-		if (list != null) {
+		if (listview != null) {
 			toast1 = Toast.makeText(getApplicationContext(),
 					"Bitte klicken Sie\ndas gewünschte Wlannetz", 10000);
 			toast1.setGravity(Gravity.CENTER, 0, 75);
 			toast1.show();
 
 		}
+
+		// listview.getOnItemLongClickListener();
 
 	}
 
@@ -90,27 +96,38 @@ public class MainActivity extends ActionBarActivity {
 		super.onResume();
 	}
 
-	class WifiScannerReceiver extends BroadcastReceiver {
+	class WifiReceiver extends BroadcastReceiver {
 		@SuppressLint("UseValueOf")
 		public void onReceive(Context c, Intent intent) {
-			List<ScanResult> wifiScanList = WifiManagerObjekt.getScanResults();
-			wifiListe = new String[wifiScanList.size()];
-			for (int i = 0; i < wifiScanList.size(); i++) {
-				wifiListe[i] = ((wifiScanList.get(i)).SSID.toString() + " " + wifiScanList
+			List<ScanResult> scanresultate = wifimanager.getScanResults();
+			wifiliste = new String[scanresultate.size()];
+			for (int i = 0; i < scanresultate.size(); i++) {
+				wifiliste[i] = ((scanresultate.get(i)).SSID.toString() + " " + scanresultate
 						.get(i).level);
 
 			}
 
-			list.setAdapter(new ArrayAdapter<String>(getApplicationContext(),
-					android.R.layout.simple_list_item_1, wifiListe));
+			listview.setAdapter(new ArrayAdapter<String>(
+					getApplicationContext(),
+					android.R.layout.simple_list_item_1, wifiliste));
 
-			if (list != null) {
+			if (listview != null) {
 				toast3 = Toast.makeText(getApplicationContext(),
 						R.string.update_info, 1000);
 				toast3.setGravity(Gravity.CENTER, 0, 75);
 				toast3.show();
 			}
 		}
+
+	}
+
+	@Override
+	public boolean onItemLongClick(AdapterView<?> parent, View view,
+			int position, long id) {
+		// TODO Auto-generated method stub
+		listview.setSelection(position);
+
+		return false;
 	}
 
 }
